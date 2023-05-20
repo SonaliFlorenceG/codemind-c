27c3924ed3eefec326e6c80a6797ab446420eca4
SonaliFlorenceG <119723770+SonaliFlorenceG@users.noreply.github.com>
2023-05-20 11:45:05 +0530

    #include<stdio.h> int main() {     int n,s=0,i,max;     float avg;     scanf("%d",&n);     int a[n];     for(i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     max=a[0];      for(i=1; i<n; i++)     {        if (max<a[i])        {            max=a[i];        }     }     printf("%d",max);
    }

---
+++ Maximum_of_an_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0,i,max;
+    float avg;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+     for(i=1; i<n; i++)
+    {
+       if (max<a[i])
+       {
+           max=a[i];
+       }
+    }
+    printf("%d",max);
+    
+  
+   
+} 
